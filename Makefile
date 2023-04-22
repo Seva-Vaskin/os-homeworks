aec979cc61e88899eace0023c319e2848655c780
Vsevolod Vaskin <vaskin.2003@gmail.com>
2023-04-22 22:15:00 +0300

    add tasks from 1st homework for tests

--- Makefile
+++ Makefile
@@ -134,6 +134,8 @@ UPROGS=\
 	$U/_wc\
 	$U/_zombie\
 	$U/_dmesg\
+	$U/_task_1_1\
+	$U/_task_1_2\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -166,7 +166,7 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     // Use num to lookup the system call function for num, call it,
     // and store its return value in p->trapframe->a0
-    pr_msg("syscall %s(%d)", sys_names[num], num);
+    pr_msg("syscall %s(%d) by proc %s(%d)", sys_names[num], num, p->name, p->pid);
     p->trapframe->a0 = syscalls[num]();
   } else {
     printf("%d %s: unknown sys call %d\n",
